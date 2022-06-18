d3e3c43cf4e3a3721a6b721f366d627e409a865f
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2022-06-18 18:56:58 +0530

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 const path = require("path");
 
 const webpackConfig = {
-	entry: path.resolve(__dirname, "src", "index.js"),
+  entry: path.resolve(__dirname, "src", "index.js"),
 
-	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "dist")
-	},
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 
-    mode: "development",
-
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
 
 module.exports = webpackConfig;
